7d15c79733d5667acbbb52586fd437d971adb312
yoshikouki <yoshikouki@pepabo.com>
2021-01-10 17:29:31 +0900

    CSVインポート機能を実装

--- マクロ.js
+++ マクロ.js
@@ -1,5 +1,6 @@
 /** @OnlyCurrentDoc */
 const dataSheetName = 'cashFlow'
+const fileManagedSheetName = 'imortedFiles'
 const dirPath = 'Money/CashFlow/'
 const filePath = '収入・支出詳細_2020-04-01_2020-04-30.csv'
 const mailAddress = 'yoshikouki@gmail.com'
@@ -8,19 +9,25 @@ function importCashFlowFromCsv() {
   // スプレッドシートとデータシートを取得
   const ss = SpreadsheetApp.getActiveSpreadsheet();
   const dataSheet = ss.getSheetByName(dataSheetName);
+  const fileManagedSheet = ss.getSheetByName(fileManagedSheetName)
   
   try {
     // csv を取得・パースして配列化
-    Logger.log('filePath: ', filePath)
     const file = DriveApp.getFilesByName(filePath).next()
-    Logger.log('file: ', file)
     const data = file.getBlob().getDataAsString("Shift_JIS")
-    Logger.log('data: ', data)
     const csv = Utilities.parseCsv(data)
-    Logger.log('csv: ', csv)
+    Logger.log('csv.firstRow: ', csv[0])
 
-    const newRow = dataSheet.getLastRow() + 1
-    dataSheet.getRange(newRow, 1).setValue(csv[1][0])
+    const latestRow = dataSheet.getLastRow()
+    Logger.log('latestRow: ', latestRow)
+
+    // 収支データのインポート処理
+    for (let r = 1; r < csv.length; r ++) {
+      for (let c = 0; c < csv[0].length; c ++) {
+        // Google スプレッドシートの列番号は 1 から始まるので CSV と差がある
+        dataSheet.getRange(r + latestRow, c + 1).setValue(csv[r][c])
+      }
+    }
   } catch(err) {
     MailApp.sendEmail(
       mailAddress, 
